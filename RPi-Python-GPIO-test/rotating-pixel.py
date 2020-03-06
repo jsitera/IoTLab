5c11c9c4601bf6e08816f7ba63806dad357ebe10
Jiri Sitera <sitera@civ.zcu.cz>
2020-03-06 19:22:12 +0100

    First example using ws281x RPi library.

---
+++ RPi-Python-GPIO-test/rotating-pixel.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# rpi_ws281x library test
+# pip install rpi_ws281x
+# https://github.com/jgarff/rpi_ws281x
+
+import time
+from rpi_ws281x import PixelStrip, Color
+import argparse
+
+# LED strip configuration:
+LED_COUNT = 60        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+
+    print('Press Ctrl-C to quit.')
+
+    color = Color(255, 0, 0)
+
+    try:
+        while True:
+            print ('Begin of loop')
+            for i in range(strip.numPixels()):
+                strip.setPixelColor(i, color)
+                strip.show()
+                time.sleep(0.2)
+                strip.setPixelColor(i, 0)
+                strip.show()
+
+    except KeyboardInterrupt:
+        colorWipe(strip, Color(0, 0, 0), 10)
